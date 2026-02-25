0ead74955f0f5a2cfbef6f53d01320fd104d6fa7
Yanet Geleta <yanetgele@gmail.com>
2026-02-25 06:59:38 +0300

    finish setting up local registration and authorization in general

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -30,8 +30,20 @@ export default User = {
   newUserGoogle: async (name, email, googleId, profileImg) => {
     // We are doing only the member ones taking the default value (will implement the other values later)
     const newUser = await db.query(
-      `inser into users (name, email, google_id, profile_img) values ($1, $2, $3, $4) returning *`,
+      `insert into users (name, email, google_id, profile_img) values ($1, $2, $3, $4) returning *`,
       [name, email, googleId, profileImg],
+    );
+    if (newUser.rows.length === 0) {
+      return null;
+    }
+    return newUser.rows[0];
+  },
+  newUserLocal: async (name, email, password, profileImg, username) => {
+    // Automatically role set as memeber
+    // Dont forget to implement the profile image path before testing
+    const newUser = await db.query(
+      `insert into users(name, email, password, profile_img, username) values ($1, $2, $3, $4, $5) returning *`,
+      [name, email, password, profileImg, username],
     );
     if (newUser.rows.length === 0) {
       return null;
